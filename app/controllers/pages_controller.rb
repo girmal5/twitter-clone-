228515de3c651e08bdc9c10f26c83b274465010b
girmal <girmal5@hotmail.com>
2018-01-15 20:31:14 +0530

    Made changes in home,profile,index mainly design views not functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
 
   def home
     @posts = Post.all
+    @newPost = Post.new
   end
 
   def profile
@@ -15,9 +16,11 @@ class PagesController < ApplicationController
     end
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id])) #shows only each users posts when clicked on profile
     @newPost = Post.new
+    @toFollow = User.all
   end
 
   def explore
     @posts = Post.all
+    @newPost = Post.new
   end
 end
